b748aa98bfb690b63ca4e7af4fe254a2e5d44024
Jan Vondrous <Jan Vondrous@localhost>
2017-09-29 16:28:52 +0200

    Greeter popis - co to ukazuje

--- greeter/src/test/java/com/quadient/academy/greeter/GreeterTest.java
+++ greeter/src/test/java/com/quadient/academy/greeter/GreeterTest.java
@@ -19,10 +19,17 @@ public class GreeterTest {
         assertThat(greeting, equalTo("Hello, John!"));
     }
 
-    //TODO
+
+    // Co si z toho odnest:
     // 1) jak vytvorit test
     //      - např. napišeme test napiseme test na Hi, a doplnime metodu
+    //         - IntellijIdea - Navigate > Test
+    //         - v hledani akci (ctrl+shift+A) "test"
+    //         - zkratka (ctrl+shift+T)
     // 2) citelnost testu - given, when, then - muze vypadat i jinak, ale jsme na testování, tak si to pojdme vyzkouset
+    //           given - v jakem stavu se nachazi to co testujeme
+    //           when  - akce ktera nastane (ta kterou testujeme)
+    //           then  - popis/overeni toho co se melo po akci stat
     // 3) assertThat - vysvetleni s ukazkami ve tride ArrayListTest
 
 }
