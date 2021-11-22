876b54244118941142b295aaf2fc7458ec25e6b9
Jan Vondrouš <j.vondrous@quadient.com>
2021-11-22 21:29:40 +0100

    Changed structure for gradle 7.2 for roulette

---
+++ roulette/lib/src/main/java/com/quadient/academy/roulette/Simulation.java
@@ -0,0 +1,43 @@
+package com.quadient.academy.roulette;
+
+public class Simulation {
+
+    private void start() {
+
+// Tohle je jen příklad. Může to vypadat úplně jinak! - Jakkoliv chcete...
+
+//        Game game = new Game();
+//        Player pepa = new PlayerBuilder().name("Pepa").money(15200);
+//        Player franta = new PlayerBuilder().name("Franta").money(4900);
+//        pepa.joinGame(game);
+//        franta.joinGame(game);
+//        pepa.bet(Board.BLACK, 500);
+//        game.crupier.throwBall();
+//        franta.bet(Board.ODD, 200);
+//        game.crupier.endOfBets();
+//        game.roulette.getNumber();
+//        game.crupier.payBets();
+//        franta.leaveGame();
+
+//        System Output:
+//        Game created - In bank is 100000
+//        Pepa joined game - he
+//        Franta joined game
+//        Pepa bet 500 to Black
+//        Ball was thrown
+//        Franta bet 200 to Odd
+//        End of bets
+//        Roulette: and the number is!  10 (black, even)
+//        Pepa won 500 - he has 15700
+//        Franta lost 200 - he has 4700
+//        In bank is 99700
+//        Franta left the table
+
+
+    }
+
+    public static void main(String[] args) {
+        Simulation app = new Simulation();
+        app.start();
+    }
+}
