cc5220fade8d35e161e0fadf2217923cabb413e3
Jan Vondrouš <j.vondrous@quadient.com>
2021-12-04 11:46:32 +0100

    GiraffeOverview - generates more complicated overview

--- giraffe-overview/lib/src/main/java/com/quadient/academy/giraffeoverview/overview/GiraffeOverview.java
+++ giraffe-overview/lib/src/main/java/com/quadient/academy/giraffeoverview/overview/GiraffeOverview.java
@@ -4,7 +4,12 @@ import com.quadient.academy.giraffeoverview.zoo.ZooInventory;
 import com.quadient.academy.giraffeoverview.zoo.animals.Giraffe;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
+
+import static java.util.Collections.emptyList;
+import static java.util.Comparator.comparingDouble;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
 public class GiraffeOverview {
     private ZooInventory zooInventory;
@@ -15,8 +20,38 @@ public class GiraffeOverview {
 
     public String generate() {
         List<Giraffe> giraffes = zooInventory.getGiraffes();
-        List<String> names = giraffes.stream().map(giraffe -> giraffe.getName()).collect(Collectors.toList());
-//        List<String> adult = giraffes.stream().map(giraffe -> giraffe.getName()).collect(Collectors.toList());
-        return "Our giraffes: " + names;
+        Giraffe tallestGiraffe = giraffes.stream().max(comparingDouble(Giraffe::getHeight)).get();
+        Map<AgeGroup, List<Giraffe>> byAgeGroup = giraffes.stream().collect(
+                groupingBy(giraffe -> {
+                            int age = giraffe.getAge();
+                            if (age <= 2) {
+                                return AgeGroup.BABY;
+                            } else if (age <= 5) {
+                                return AgeGroup.YOUNG;
+                            } else {
+                                return AgeGroup.ADULT;
+                            }
+                        }
+                ));
+
+        return "All our giraffes: " + toNames(giraffes) + "\n" +
+                "The tallest giraffe is: " + tallestGiraffe.getName() + "\n" +
+                "Baby giraffes: " + toNames(byAgeGroup.get(AgeGroup.BABY)) + "\n" +
+                "Young giraffes: " + toNames(byAgeGroup.get(AgeGroup.YOUNG)) + "\n" +
+                "Adult giraffes: " + toNames(byAgeGroup.get(AgeGroup.ADULT))
+                ;
+    }
+
+    private List<String> toNames(List<Giraffe> giraffes) {
+        if (giraffes == null) {
+            return emptyList();
+        }
+        return giraffes.stream().map(Giraffe::getName).collect(toList());
+    }
+
+    private enum AgeGroup {
+        BABY,
+        YOUNG,
+        ADULT,
     }
 }
