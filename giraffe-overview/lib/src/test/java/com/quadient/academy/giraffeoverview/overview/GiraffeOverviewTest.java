6340fd216faf4b8735da9bb9ce4977bcf167031c
Jan Vondrouš <j.vondrous@quadient.com>
2021-12-04 14:29:20 +0100

    GirrafeOverview - notes to solution

--- giraffe-overview/lib/src/test/java/com/quadient/academy/giraffeoverview/overview/GiraffeOverviewTest.java
+++ giraffe-overview/lib/src/test/java/com/quadient/academy/giraffeoverview/overview/GiraffeOverviewTest.java
@@ -64,7 +64,7 @@ class GiraffeOverviewTest {
         // then
         assertThat(overview).contains("All our giraffes: [Majda, Aja]");
 
-        //Některé problémy takového testu:
+        //Některé problémy takového testu (zkusme dát dohromady):
         //        - psát to je hrozný
         //           - Navíc tady ostatní zvířata jsou bez parametrů, ale normálně by měli hromadu parametrů (a to navytvářet by bylo o hoodně horší)
         //           - Navíc by tam bylo ještě víc tranzitivních závislostí
@@ -79,9 +79,12 @@ class GiraffeOverviewTest {
     }
 
 
-    //Je jiné řešení?
-    // Ano několik - nás ale bude zajímat jen https://site.mockito.org/
-
+    // Je jiné řešení?
+    //   - Ano několik - nás ale bude zajímat jen https://site.mockito.org/
+    // Dokumentace - https://javadoc.io/doc/org.mockito/mockito-core/latest/org/mockito/Mockito.html
+    //    - možná se Vám nezdá formát, ale věřte mi, že tenhle návod je opravdu velmi velmi dobře sestaven
+    //    - možná se Vám zdá dlouhý, ale Vám na základní práci bude stačit si vyzkoušet několik prvních příkladů
+    //       - další můžete přibírat až při složitějším mockování, nebo když je potřeba
 
 
     @Test
@@ -101,5 +104,46 @@ class GiraffeOverviewTest {
         // then
         assertThat(overview).contains("All our giraffes: [Majda, Aja]");
     }
+
+
+
+
+    // Poznámky k řešení s mockem (zkusme dát dohromady):
+    // - Teď testujeme třídu GiraffeOverview proti "interfacu" třídy ZooInventory
+    // - Změny ve validacích ve třídě ZooInventory na nás nemají vliv
+    // - změny u ostatních zvířat na tento test nemají vliv
+    //     - změnám ve třídě Giraffe by se dalo vynnout dalším mockem (ale není tu nutné Vás tím zbytečně mást)
+    // => Test je mnohem robusnější a mnohem více testuje pouze třídu GiraffeOverview
+
+
+    // Poznámka k rychlosti mocků
+    //  - Je tam menší zpomalení, ale je to velmi velmi rychlé
+    //    - v 99% všecho co kdy budete testovat je toto zpomalení zanedbatelné
+    //  - při psaní aplikací mocky dokážou ušetřit spoustu práce
+    //     - usnadní návrh tříd
+    //     - usnadní budoucí změny
+    //     - usnadní udržitelnost testů
+
+
+
+
+
+
+
+    // Procvičit psaní vlastních mocků
+
+
+
+
+
+
+
+
+    // Závěr:
+    // - víme co je mock a na co se dá použít
+    // - víme, že existuje mockito.
+    // - umíme použít mockito na základní jednoduché mockování
+    // - víme, kde se o mockitu dozvědět víc
+    //   - Návod na mockito https://javadoc.io/doc/org.mockito/mockito-core/latest/org/mockito/Mockito.html
 
 }
